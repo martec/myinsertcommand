2d9ff7b8902381fcc7c68d1ef5101f4c3d2cdd8e
martec <mr.martchero@gmail.com>
2014-07-27 15:41:35 +0900

    fixes intendation

--- inc/plugins/myinsertcommand.php
+++ inc/plugins/myinsertcommand.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 // Disallow direct access to this file for security reasons
 if(!defined("IN_MYBB"))
 {
@@ -7,45 +7,40 @@ if(!defined("IN_MYBB"))
 
 if(!defined("PLUGINLIBRARY"))
 {
-    define("PLUGINLIBRARY", MYBB_ROOT."inc/plugins/pluginlibrary.php");
+	define("PLUGINLIBRARY", MYBB_ROOT."inc/plugins/pluginlibrary.php");
 }
 
 function myinsertcommand_info()
 {
-    global $mybb, $plugins_cache;
+	global $mybb, $plugins_cache;
 
 	$info = array(
-		'name'          => 'My Insert Command',
-		'description'   => 'Insert new command in Sceditor',
-		'website'       => '',
-		'author'        => 'martec',
-		'authorsite'    => '',
-		'version'       => '0.1',
-		'guid'          => '',
+		'name'			=> 'My Insert Command',
+		'description'	=> 'Insert new command in Sceditor',
+		'website'		=> '',
+		'author'		=> 'martec',
+		'authorsite'	=> '',
+		'version'		=> '0.1.1',
+		'guid'			=> '',
 		'compatibility' => '17*,18*'
 	);
 
-    if(myinsertcommand_is_installed() && $plugins_cache['active']['myinsertcommand'])
-    {
-        global $PL;
-        $PL or require_once PLUGINLIBRARY;
+	if(myinsertcommand_is_installed() && $plugins_cache['active']['myinsertcommand'])
+	{
+		global $PL;
+		$PL or require_once PLUGINLIBRARY;
 
-        $editcode = $PL->url_append("index.php?module=config-plugins",
-                                   array("myinsertcommand" => "edit",
-                                         "my_post_key" => $mybb->post_code));
-        $undocode = $PL->url_append("index.php",
-                                   array("module" => "config-plugins",
-                                         "myinsertcommand" => "undo",
-                                         "my_post_key" => $mybb->post_code));
+		$editcode = $PL->url_append("index.php?module=config-plugins", array("myinsertcommand" => "edit", "my_post_key" => $mybb->post_code));
+		$undocode = $PL->url_append("index.php", array("module" => "config-plugins", "myinsertcommand" => "undo", "my_post_key" => $mybb->post_code));
 
-        $editcode = "index.php?module=config-plugins&amp;myinsertcommand=edit&amp;my_post_key=".$mybb->post_code;
-        $undocode = "index.php?module=config-plugins&amp;myinsertcommand=undo&amp;my_post_key=".$mybb->post_code;
+		$editcode = "index.php?module=config-plugins&amp;myinsertcommand=edit&amp;my_post_key=".$mybb->post_code;
+		$undocode = "index.php?module=config-plugins&amp;myinsertcommand=undo&amp;my_post_key=".$mybb->post_code;
 
-        $info["description"] .= "<br /><a href=\"{$editcode}\">Make edits to inc/functions.php</a>.";
-        $info["description"] .= "    | <a href=\"{$undocode}\">Undo edits to inc/functions.php</a>.";
-    }
+		$info["description"] .= "<br /><a href=\"{$editcode}\">Make edits to inc/functions.php</a>.";
+		$info["description"] .= "	 | <a href=\"{$undocode}\">Undo edits to inc/functions.php</a>.";
+	}
 
-    return $info;	
+	return $info;
 }
 
 function myinsertcommand_install()
@@ -57,10 +52,10 @@ function myinsertcommand_install()
 		flash_message("PluginLibrary is missing.", "error");
 		admin_redirect("index.php?module=config-plugins");
 	}
-	
+
 	$lang->load('config_myinsertcommand');
 
-	$query  = $db->simple_select("settinggroups", "COUNT(*) as rows");
+	$query	= $db->simple_select("settinggroups", "COUNT(*) as rows");
 	$dorder = $db->fetch_field($query, 'rows') + 1;
 
 	$groupid = $db->insert_query('settinggroups', array(
@@ -101,43 +96,43 @@ function myinsertcommand_uninstall()
 	$db->write_query("DELETE FROM " . TABLE_PREFIX . "settings WHERE name IN('myinsertcommand_rules')");
 	$db->delete_query("settinggroups", "name = 'myinsertcommand'");
 
-    global $PL;
-    $PL or require_once PLUGINLIBRARY;
+	global $PL;
+	$PL or require_once PLUGINLIBRARY;
 
-	$PL->edit_core("myinsertcommand", "inc/functions.php", array(), true);	
+	$PL->edit_core("myinsertcommand", "inc/functions.php", array(), true);
 }
 
 function myinsertcommand_activate()
 {
 	global $db;
-	
+
 	if(!file_exists(PLUGINLIBRARY))
 	{
 		flash_message("PluginLibrary is missing.", "error");
 		admin_redirect("index.php?module=config-plugins");
-	}	
-	
+	}
+
 	include_once MYBB_ROOT."inc/adminfunctions_templates.php";
-	
-    find_replace_templatesets(
-        'codebuttons',
-        '#' . preg_quote('<link rel="stylesheet" href="{$mybb->asset_url}/jscripts/sceditor/editor_themes/{$theme[\'editortheme\']}" type="text/css" media="all" />') . '#i',
-        '<link rel="stylesheet" href="{$mybb->asset_url}/jscripts/sceditor/editor_themes/{$theme[\'editortheme\']}" type="text/css" media="all" />
+
+	find_replace_templatesets(
+		'codebuttons',
+		'#' . preg_quote('<link rel="stylesheet" href="{$mybb->asset_url}/jscripts/sceditor/editor_themes/{$theme[\'editortheme\']}" type="text/css" media="all" />') . '#i',
+		'<link rel="stylesheet" href="{$mybb->asset_url}/jscripts/sceditor/editor_themes/{$theme[\'editortheme\']}" type="text/css" media="all" />
 <link rel="stylesheet" href="{$mybb->asset_url}/jscripts/sceditor/myinsertcommand/myinsertcommand.css" type="text/css" media="all" />'
-    );
+	);
 
-    find_replace_templatesets(
-        'codebuttons',
-        '#' . preg_quote('<script type="text/javascript">') . '#i',
-        '{$insert_func}
+	find_replace_templatesets(
+		'codebuttons',
+		'#' . preg_quote('<script type="text/javascript">') . '#i',
+		'{$insert_func}
 <script type="text/javascript">'
-    );
+	);
 
-    find_replace_templatesets(
-        'codebuttons',
-        '#' . preg_quote('quote|') . '#i',
-        'quote|{$insert}'
-    );
+	find_replace_templatesets(
+		'codebuttons',
+		'#' . preg_quote('quote|') . '#i',
+		'quote|{$insert}'
+	);
 
 	$template = array(
 		"tid"		=> NULL,
@@ -194,12 +189,12 @@ function myinsertcommand_activate()
 
 				if (html) {
 					before = before + html + end;
-					end    = null;
+					end	   = null;
 				}
 
 				editor.insert(before, end);
 				editor.closeDropDown(true);
-				e.preventDefault();			
+				e.preventDefault();
 			});
 
 			editor.createDropDown(caller, \'insertcommand\', content);
@@ -217,8 +212,8 @@ function myinsertcommand_activate()
 	);
 	$db->insert_query("templates", $template);
 
-    global $PL;
-    $PL or require_once PLUGINLIBRARY;
+	global $PL;
+	$PL or require_once PLUGINLIBRARY;
 
 	$PL->edit_core("myinsertcommand", "inc/functions.php",
 		array(	'search' => array('$sourcemode = "MyBBEditor.sourceMode(true);";', '}'),
@@ -246,52 +241,51 @@ function myinsertcommand_deactivate()
 {
 	global $db;
 	include_once MYBB_ROOT."inc/adminfunctions_templates.php";
-	
-    find_replace_templatesets(
-        'codebuttons',
-        '#' . preg_quote('<link rel="stylesheet" href="{$mybb->asset_url}/jscripts/sceditor/editor_themes/{$theme[\'editortheme\']}" type="text/css" media="all" />
-<link rel="stylesheet" href="{$mybb->asset_url}/jscripts/sceditor/myinsertcommand/myinsertcommand.css" type="text/css" media="all" />') . '#i',
-        '<link rel="stylesheet" href="{$mybb->asset_url}/jscripts/sceditor/editor_themes/{$theme[\'editortheme\']}" type="text/css" media="all" />'
-    );
-	
-    find_replace_templatesets(
-        'codebuttons',
-        '#' . preg_quote('{$insert_func}
-<script type="text/javascript">') . '#i',
-        '<script type="text/javascript">'
-    );
 
-    find_replace_templatesets(
-        'codebuttons',
-        '#' . preg_quote('quote|{$insert}') . '#i',
-        'quote|'
-    );
+	find_replace_templatesets(
+		'codebuttons',
+		'#' . preg_quote('<link rel="stylesheet" href="{$mybb->asset_url}/jscripts/sceditor/editor_themes/{$theme[\'editortheme\']}" type="text/css" media="all" />
+<link rel="stylesheet" href="{$mybb->asset_url}/jscripts/sceditor/myinsertcommand/myinsertcommand.css" type="text/css" media="all" />') . '#i',
+		'<link rel="stylesheet" href="{$mybb->asset_url}/jscripts/sceditor/editor_themes/{$theme[\'editortheme\']}" type="text/css" media="all" />'
+	);
+
+	find_replace_templatesets(
+		'codebuttons',
+		'#' . preg_quote('{$insert_func}
+<script type="text/javascript">') . '#i',
+		'<script type="text/javascript">'
+	);
+
+	find_replace_templatesets(
+		'codebuttons',
+		'#' . preg_quote('quote|{$insert}') . '#i',
+		'quote|'
+	);
 
 	$db->query("DELETE FROM ".TABLE_PREFIX."templates WHERE title='insert_button'");
 
-    global $PL;
-    $PL or require_once PLUGINLIBRARY;
+	global $PL;
+	$PL or require_once PLUGINLIBRARY;
 
 	$PL->edit_core("myinsertcommand", "inc/functions.php", array(), true);
-
 }
 
 $plugins->add_hook("admin_config_plugins_begin", "myinsertcommand_edit");
 function myinsertcommand_edit()
 {
-    global $mybb;
+	global $mybb;
 
-    if($mybb->input['my_post_key'] != $mybb->post_code)
-    {
-        return;
-    }
+	if($mybb->input['my_post_key'] != $mybb->post_code)
+	{
+		return;
+	}
 
-    global $PL;
-    $PL or require_once PLUGINLIBRARY;
-	
-    if($mybb->input['myinsertcommand'] == 'edit')
-    {
-        $result = $PL->edit_core("myinsertcommand", "inc/functions.php",
+	global $PL;
+	$PL or require_once PLUGINLIBRARY;
+
+	if($mybb->input['myinsertcommand'] == 'edit')
+	{
+		$result = $PL->edit_core("myinsertcommand", "inc/functions.php",
 			array(	'search' => array('$sourcemode = "MyBBEditor.sourceMode(true);";', '}'),
 					'after' => array(
 					'
@@ -311,29 +305,29 @@ function myinsertcommand_edit()
 			),
 			true
 		);
-    }
+	}
 
-    else if($mybb->input['myinsertcommand'] == 'undo')
-    {
-        $result = $PL->edit_core("myinsertcommand", "inc/functions.php", array(), true);
-    }
+	else if($mybb->input['myinsertcommand'] == 'undo')
+	{
+		$result = $PL->edit_core("myinsertcommand", "inc/functions.php", array(), true);
+	}
 
-    else
-    {
-        return;
-    }
+	else
+	{
+		return;
+	}
 
-    if($result === true)
-    {
-        flash_message("The file inc/functions.php was modified successfully.", "success");
-        admin_redirect("index.php?module=config-plugins");
-    }
+	if($result === true)
+	{
+		flash_message("The file inc/functions.php was modified successfully.", "success");
+		admin_redirect("index.php?module=config-plugins");
+	}
 
-    else
-    {
-        flash_message("The file inc/functions.php could not be edited. Are the CHMOD settings correct?", "error");
-        admin_redirect("index.php?module=config-plugins");
-    }
+	else
+	{
+		flash_message("The file inc/functions.php could not be edited. Are the CHMOD settings correct?", "error");
+		admin_redirect("index.php?module=config-plugins");
+	}
 }
 
 ?>
